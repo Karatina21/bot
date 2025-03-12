b0abc59166c9e07e9925affd8abcbdd96ce7e925
Karatina21 <gainwithpk@gmail.com>
2025-03-12 18:54:58 +0300

    Update site-config.ts

--- src/utils/site-config.ts
+++ src/utils/site-config.ts
@@ -1,4 +1,4 @@
-export const website_domain = 'bot.binaryfx.site';
-export const website_name = 'Binaryfx';
+export const website_domain = 'bot.binarymafia.site';
+export const website_name = 'binarymafia';
 export const default_title = website_name;
 export const TRACKING_STATUS_KEY = 'tracking_status';
